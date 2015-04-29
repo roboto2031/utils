f19c1875b0bbf843010b12c3175aabe6a3a1291e
roboto2031 <roboto2031@gmail.com>
2015-04-28 21:22:48 -0700

    basic logic

--- pyutils/dns_exfil_svr.py
+++ pyutils/dns_exfil_svr.py
@@ -3,3 +3,15 @@
 # listen for dns requests for various subdomains
 # take subdomain and piece together the file.
 
+import socket
+import sys
+
+domain="iamev.il";
+request="";
+subdomain="";
+response="127.0.0.1"
+
+if domain in request:
+	subdomain=request.split('.');
+else:
+	response=gethostbyname(request);
