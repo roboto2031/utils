1da702a9c68d33efe3b0de1df3c431cb605d4cca
roboto2031 <roboto2031@gmail.com>
2016-12-07 15:47:57 -0600

    update

--- nexpose/sitelist.py
+++ nexpose/sitelist.py
@@ -1,3 +1,30 @@
 #! /usr/bin/python
 
+import urllib,
+import getopt, sys, re, lxml
 
+
+userid
+password
+sessionid
+#192.168.1.31
+
+def usage():
+	print("\n============================================================================================")
+	print("Usage:")
+	print("sitelist.py -l {login} -p {password}")
+	print("-------------")
+	print("-l | --key <API KEY>		        ### Shodan API key")
+	print("-p | --page <page>		        ### result page, (default)first page, or all pages") 
+	print("============================================================================================\n")
+
+def login():
+#<?xml version="1.0" encoding="UTF-8"?>
+#<LoginRequest user-id="nxadmin" password="nxadmin" />
+	url=
+	data = urllib.parse.urlencode(
+
+
+def logout():
+
+def main():
